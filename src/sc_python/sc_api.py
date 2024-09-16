8ebec1127a5538aa6a6f41eb802f7e858b35be09
Rob Hamilton <berthamilton@gmail.com>
2024-09-16 17:37:08 +0100

    test coverage

---
+++ src/sc_python/sc_api.py
@@ -0,0 +1,12 @@
+''' ScApi '''
+
+
+class ScApi:
+    ''' TODO '''
+    def __init__(self):
+        ''' TODO '''
+        pass
+
+    def is_valid(self):
+        ''' :return true '''
+        return True
---
+++ test/test_sc_api.py
@@ -0,0 +1,6 @@
+''' TODO '''
+from sc_python import sc_api
+
+def test_sc_api_is_valid():
+    ''' TODO '''
+    assert sc_api.ScApi().is_valid()
