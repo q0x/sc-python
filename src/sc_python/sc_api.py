ee0ecdf3d4ba95e0c8d6e4c6c75dbdacbdbb2c63
Rob Hamilton <berthamilton@gmail.com>
2024-09-17 17:43:11 +0100

    created a working test

--- src/sc_python/sc_api.py
+++ src/sc_python/sc_api.py
@@ -1,12 +1,15 @@
 ''' ScApi '''
-
+import os
+from sonarqube import SonarQubeClient
 
 class ScApi:
     ''' TODO '''
     def __init__(self):
         ''' TODO '''
-        pass
+        url = os.getenv('SONAR_HOST_URL','http://sonarcloud.io')
+        token = os.getenv('SONAR_TOKEN')
+        self._client = SonarQubeClient(sonarqube_url=url, token=token)
 
     def is_valid(self):
-        ''' :return true '''
-        return True
+        ''' :return check_credentials '''
+        return self._client.auth.check_credentials()
